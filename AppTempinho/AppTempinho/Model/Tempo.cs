5ef8b1edf30e7e084129e85979659cb24fa0f56d
Usuario <Usuario@CPD03-M02>
2022-09-27 12:17:16 -0300

    Adicionar arquivos de projeto.

---
+++ AppTempinho/AppTempinho/Model/Tempo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTempinho.Model
+{
+    class Tempo
+    {
+        public string Titulo { get; set; }
+        public string Temperatura { get; set; }
+        public string Vento { get; set; }
+        public string Humidade { get; set; }
+        public string Visibilidade { get; set; }
+        public string NascerSol { get; set; }
+
+        public string Porsol { get; set; }
+
+        public Tempo()
+        {
+            this.Titulo = "";
+            this.Temperatura = "";
+            this.Vento = "";
+            this.Humidade = "";
+            this.Visibilidade = "";
+            this.NascerSol = "";
+            this.Porsol = "";
+
+
+        }
+    }
+
+}
---
+++ AppTempinho/AppTempinho/Services/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppTempinho.Model;
+
+namespace AppTempinho.Services
+{
+    class DataService
+    {
+        public static async Task<Tempo> GetPrevisaoDoTempo(string cidade)
+        {
+            string appId = "55f956f2188a48e78195d2bfa3018ebe";
+
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric" + "&appid=" + appId;
+            dynamic resultado = await getDataFromSrevice(queryString).ConfigureAwait(false);
+
+        }
+    }
+}
